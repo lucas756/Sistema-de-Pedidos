c2dad08077f78ca972df7c1b303ed38716b923b2
Lucas Salles <salleslucas00@gmail.com>
2020-10-19 19:42:15 -0300

    deploy

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: 'root',
-  database: 'base_pedidos',
+  host: 'us-cdbr-east-02.cleardb.com',
+  username: 'b37f9704709c60',
+  password: '2d9e3514',
+  database: 'heroku_0852980fee7f6d5',
   define: {
     timestamps: true,
     underscored: true,
